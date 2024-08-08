61f59634764799c0a7568f4078f3221ee70dc0dd
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-08-08 13:30:06 +0000

    updating file 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -7,26 +7,26 @@
  */
 void selection_sort(int *array, size_t size)
 {
-	int i, j, jMin, temp;
-	int length = size;
+	size_t i, j, jMin, temp;
 
 	if (array == NULL)
 		return;
 	
-	for (i = 0; i < length - 1; i++)
+	for (i = 0; i < size; i++)
 	{
 		jMin = i;
 
-		for (j = i + 1; j < length; j++)
+		for (j = i + 1; j < size; j++)
 		{
-			if (array[i] < array[jMin])
+			if (array[j] < array[jMin])
 				jMin = j;
 		}
-	}
-	if (jMin != i)
-	{
-		temp = array[jMin];
-		array[jMin] = array[i];
-		array[i] = temp;
+		if (jMin != i)
+		{
+			temp = array[jMin];
+			array[jMin] = array[i];
+			array[i] = temp;
+			print_array(array, size);
+		}
 	}
 }
