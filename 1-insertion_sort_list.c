257163e8b4d1b608f8ea468370ae77c8bbcf9679
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-08-08 11:14:30 +0000

    add files t1

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,47 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - function for use insertion sort
+ * @list: list of node
+ * Return: return void
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current;
+	listint_t *tmp;
+
+	if (list == NULL)
+		return;
+
+	if ((*list)->next == NULL)
+		return;
+
+	current = (*list)->next;
+
+	while (current != NULL)
+	{
+		tmp = current->prev;
+
+		while (tmp != NULL &&
+				current->n < tmp->n)
+		{
+			if (tmp->prev != NULL)
+				tmp->prev->next = current;
+			else
+				*list = current;
+
+			if (current->next != NULL)
+				current->next->prev = tmp;
+
+			current->prev = tmp->prev;
+			tmp->next = current->next;
+			current->next = tmp;
+			tmp->prev = current;
+
+			tmp = current->prev;
+			print_list(*list);
+		}
+		current = current->next;
+	}
+}
